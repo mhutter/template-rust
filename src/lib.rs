0c7277d9aa34baec65cf881fb47ed27f2ef09140
Manuel Hutter <manuel@hutter.io>
2024-08-26 11:18:35 +0200

    Initial import :seedling:
    Signed-off-by: Manuel Hutter <manuel@hutter.io>

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+#![forbid(unsafe_code)]
+
+pub const APP: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"),);
+
+pub const APP_UA: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    "/",
+    env!("CARGO_PKG_VERSION"),
+    " (",
+    env!("CARGO_PKG_REPOSITORY"),
+    ")",
+);
